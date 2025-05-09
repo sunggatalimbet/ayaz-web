12cd6e07ab9bd206b7d2a1ec269562e77050645c
Sunggat Alimbetov <sunggat@greyball.com>
2025-05-10 03:02:29 +0500

    fix: remove initialized components

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,11 +14,11 @@ export default defineConfig({
   ],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "~": resolve(__dirname, "./src"),
     },
   },
-})
+});
